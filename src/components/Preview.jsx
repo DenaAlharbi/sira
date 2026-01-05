1cdda0e60121bae3c845cdbcfa9f8998487601c0
DenaAlharbi <s202250560@kfupm.edu.sa>
2026-01-05 05:18:33 +0300

    error fixed

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import BasicFree from '../templates/BasicFree/index';
+import BasicFree from '../templates/BasicFree/Index';
 
 const templateMap = {
   'BasicFree': BasicFree,
